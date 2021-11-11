f0556cda4ca51ade8fc87cc64f948d8ee33c5dc3
Zachary Haggard <37089160+AgentUkulele@users.noreply.github.com>
2021-11-11 14:28:09 -0500

    :)

---
+++ everything_but_in_c/5.8/FivePointEight.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char** argv) {
+    int *v;
+
+    v = malloc(sizeof(int));
+
+    for (int i = 0; i < 4; i++) {
+        printf("\nEnter number %d: ", i+1);
+        scanf("%d", &v[i]);
+    }
+
+    for (int i = 1; i < 4; i++) {
+        if (v[i] != v[i-1]) {
+            printf("\nNot two pairs");
+            return 0;
+        } 
+    }
+
+    free(v);
+
+    printf("\nTwo pairs");
+    return 0;
+
+
+}
