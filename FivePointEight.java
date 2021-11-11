5e0adb20818041b2d3d61a7d713589952f7162f9
Zachary Haggard <37089160+AgentUkulele@users.noreply.github.com>
2021-11-11 11:01:12 -0500

    Create FivePointEight.java
    im gonna shoot myself

---
+++ FivePointEight.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+import java.lang.System.exit;
+
+public class FivePointEight {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter four numbers separated by spaces: ");
+    String[] in = sc.next().split("");
+    
+    String c;
+    for (int i = 1; i < 4; i++) {
+      if (in[i] != in[i-1])
+      {
+        System.out.println("not two pairs");
+        exit(1);
+      }
+    }
+    
+    System.out.println("two pairs");
+  }
+}
